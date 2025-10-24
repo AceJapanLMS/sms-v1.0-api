0a8718e74b0e1d20f26381d241c67d0882f0eeb6
MG <akog02@gmail.com>
2025-10-24 16:14:44 +0700

    Update tables-create.sql
    updates table scripts

--- sms-api-v1/scripts/tables-create.sql
+++ sms-api-v1/scripts/tables-create.sql
@@ -11,3 +11,19 @@ CREATE TABLE school_infos(
     created_at timestamp NULL DEFAULT NULL,
     updated_at timestamp NULL DEFAULT NULL
 );
+
+ALTER TABLE users ADD school_info_id BIGINT  NULL;
+
+CREATE TABLE school_users(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    school_info_id BIGINT  NOT NULL,
+    user_id BIGINT UNSIGNED NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    otp CHAR(6) NOT NULL,
+    expired_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NULL DEFAULT NULL,
+    updated_at TIMESTAMP NULL DEFAULT NULL,
+    FOREIGN KEY (school_info_id) REFERENCES school_infos(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
